0206e549ca5809d888272a53a0f3a6c3edffa5cf
karunialeo <karunialeo@gmail.com>
2022-03-26 00:18:09 +0700

    ready for deploy

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
 const router = require("./src/routes");
-const app = express();
-const port = 5000;
-const cors = require("cors");
+const port = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
 app.use("/api/v1/", router);
 
-app.use("/uploads", express.static("uploads"));
+app.get("/", function (req, res) {
+  res.send({
+    message: "Hello World",
+    CLIENT_URL: process.env.CLIENT_URL,
+  });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
