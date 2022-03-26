d6805f9eca31240cce99844bf8bd9d1882b21b3c
karunialeo <karunialeo@gmail.com>
2022-03-26 21:02:11 +0700

    connect addTransaction to cloudinary

--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -1,227 +1,242 @@
-const {tb_user, tb_transaction, tb_order } = require('../../models')
+const { tb_user, tb_transaction, tb_order } = require("../../models");
 
 exports.addTransaction = async (req, res) => {
-    try {
-        const { data } = req.body;
+  try {
+    const { data } = req.body;
 
-        // code here
-        let newTransaction = await tb_transaction.create({
-            ...data,
-            fullname: req.body.fullname,
-            email: req.body.email,
-            phone: req.body.phone,
-            address: req.body.address,
-            idUser: req.tb_user.id,
-            totalPrice: req.body.totalPrice,
-            attachment: req.file.filename,
-            status: 'Waiting Approve',
-        })
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "waysbucks_media",
+      use_filename: true,
+      unique_filename: false,
+    });
 
-        await tb_order.update({ 
-            status: 'process'
-        }, {
-            where: {
-                idUser: req.tb_user.id,
-                status: 'active',
-            },
-        });
+    // code here
+    let newTransaction = await tb_transaction.create({
+      ...data,
+      fullname: req.body.fullname,
+      email: req.body.email,
+      phone: req.body.phone,
+      address: req.body.address,
+      idUser: req.tb_user.id,
+      totalPrice: req.body.totalPrice,
+      attachment: result.public_id,
+      status: "Waiting Approve",
+    });
 
-        res.send({
-            status: "Success...",
-            message: "Add Transaction Successfully",
-            data: {
-                newTransaction,
-            },
-        })
-    } catch (error) {
-        res.status(500).send({
-            status: "Failed",
-            message: "Server Error",
-        });
-        console.log(error)
-    }
-}
+    await tb_order.update(
+      {
+        status: "process",
+      },
+      {
+        where: {
+          idUser: req.tb_user.id,
+          status: "active",
+        },
+      }
+    );
+
+    res.send({
+      status: "Success...",
+      message: "Add Transaction Successfully",
+      data: {
+        newTransaction,
+      },
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: "Failed",
+      message: "Server Error",
+    });
+    console.log(error);
+  }
+};
 
 exports.getTransactions = async (req, res) => {
-    try {
-        let data = await tb_transaction.findAll({
-            // include: [
-            //   {
-            //     model: tb_user,
-            //     as: "user",
-            //     attributes: {
-            //       exclude: ["createdAt", "updatedAt", "password"],
-            //     },
-            //   },
-            // ],
-            attributes: {
-                exclude: ["createdAt", "updatedAt", "idUser"],
-            },
-        });
-    
-        data = JSON.parse(JSON.stringify(data))
-    
-        data = data.map((item) => {
-            return {
-                ...item,
-                attachment: process.env.FILE_PATH + item.attachment
-            }
-        })
-    
-        res.send({
-            status: "Success on Getting Transactions",
-            transactions: data,
-        });
-    } catch (error) {
-        res.send({
-        status: "Failed",
-        message: "Server Error",
-        });
-    }
+  try {
+    let data = await tb_transaction.findAll({
+      // include: [
+      //   {
+      //     model: tb_user,
+      //     as: "user",
+      //     attributes: {
+      //       exclude: ["createdAt", "updatedAt", "password"],
+      //     },
+      //   },
+      // ],
+      attributes: {
+        exclude: ["createdAt", "updatedAt", "idUser"],
+      },
+    });
+
+    data = JSON.parse(JSON.stringify(data));
+
+    data = data.map((item) => {
+      return {
+        ...item,
+        attachment: process.env.FILE_PATH + item.attachment,
+      };
+    });
+
+    res.send({
+      status: "Success on Getting Transactions",
+      transactions: data,
+    });
+  } catch (error) {
+    res.send({
+      status: "Failed",
+      message: "Server Error",
+    });
+  }
 };
 
 exports.getTransaction = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        let data = await tb_transaction.findOne({
-            where: {
-                idUser: id
-            },
-            include: [
-              {
-                model: tb_user,
-                as: "user",
-                attributes: {
-                  exclude: ["createdAt", "updatedAt", "password"],
-                },
-              },
-            ],
-            attributes: {
-                exclude: ["createdAt", "updatedAt", "idUser"],
-            },
-        });
-    
-        data = JSON.parse(JSON.stringify(data))
-    
-        data.attachment = process.env.FILE_PATH + data.attachment
-    
-        res.send({
-            status: "Success on Getting Transaction",
-            transaction: data,
-        });
-    } catch (error) {
-        res.send({
-        status: "Failed",
-        message: "Server Error",
-        });
-    }
+    let data = await tb_transaction.findOne({
+      where: {
+        idUser: id,
+      },
+      include: [
+        {
+          model: tb_user,
+          as: "user",
+          attributes: {
+            exclude: ["createdAt", "updatedAt", "password"],
+          },
+        },
+      ],
+      attributes: {
+        exclude: ["createdAt", "updatedAt", "idUser"],
+      },
+    });
+
+    data = JSON.parse(JSON.stringify(data));
+
+    data.attachment = process.env.FILE_PATH + data.attachment;
+
+    res.send({
+      status: "Success on Getting Transaction",
+      transaction: data,
+    });
+  } catch (error) {
+    res.send({
+      status: "Failed",
+      message: "Server Error",
+    });
+  }
 };
 
 exports.updateTransaction = async (req, res) => {
-    try {
-        const { id } = req.params;
-    
-        await tb_transaction.update(req.body, {
-            where: {
-                id,
-            },
-        });
-        
-        res.send({
-            status: "Success...",
-            message: `Transaction with ID: ${id} updated`,
-            data: req.body,
-        });
-    } catch (error) {
-      console.log(error);
-      res.send({
-        status: "Failed",
-        message: "Server Error",
-      });
-    }
+  try {
+    const { id } = req.params;
+
+    await tb_transaction.update(req.body, {
+      where: {
+        id,
+      },
+    });
+
+    res.send({
+      status: "Success...",
+      message: `Transaction with ID: ${id} updated`,
+      data: req.body,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: "Failed",
+      message: "Server Error",
+    });
+  }
 };
 
 exports.finishTransaction = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const transaction = await tb_transaction.findOne({
-            where: {
-              id,
-            },
-            attributes: {
-              exclude: ["createdAt", "updatedAt"],
-            },
-          })
-    
-        await tb_transaction.update(req.body, {
-            where: {
-                id,
-            },
-        });
+    const transaction = await tb_transaction.findOne({
+      where: {
+        id,
+      },
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
 
-        await tb_order.update({ 
-            status: 'success'
-        }, {
-            where: {
-                idUser: transaction.idUser,
-                status: 'process',
-            },
-        });
-        
-        res.send({
-            status: "Success...",
-            message: `Transaction with ID: ${id} success`,
-            data: req.body,
-        });
-    } catch (error) {
-      console.log(error);
-      res.send({
-        status: "Failed",
-        message: "Server Error",
-      });
-    }
+    await tb_transaction.update(req.body, {
+      where: {
+        id,
+      },
+    });
+
+    await tb_order.update(
+      {
+        status: "success",
+      },
+      {
+        where: {
+          idUser: transaction.idUser,
+          status: "process",
+        },
+      }
+    );
+
+    res.send({
+      status: "Success...",
+      message: `Transaction with ID: ${id} success`,
+      data: req.body,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: "Failed",
+      message: "Server Error",
+    });
+  }
 };
 
 exports.cancelTransaction = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const transaction = await tb_transaction.findOne({
-            where: {
-              id,
-            },
-            attributes: {
-              exclude: ["createdAt", "updatedAt"],
-            },
-          })
-    
-        await tb_transaction.update(req.body, {
-            where: {
-                id,
-            },
-        });
+    const transaction = await tb_transaction.findOne({
+      where: {
+        id,
+      },
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
 
-        await tb_order.update({ 
-            status: 'cancel'
-        }, {
-            where: {
-                idUser: transaction.idUser,
-                status: 'process',
-            },
-        });
-        
-        res.send({
-            status: "Success...",
-            message: `Transaction with ID: ${id} canceled`,
-            data: req.body,
-        });
-    } catch (error) {
-      console.log(error);
-      res.send({
-        status: "Failed",
-        message: "Server Error",
-      });
-    }
+    await tb_transaction.update(req.body, {
+      where: {
+        id,
+      },
+    });
+
+    await tb_order.update(
+      {
+        status: "cancel",
+      },
+      {
+        where: {
+          idUser: transaction.idUser,
+          status: "process",
+        },
+      }
+    );
+
+    res.send({
+      status: "Success...",
+      message: `Transaction with ID: ${id} canceled`,
+      data: req.body,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: "Failed",
+      message: "Server Error",
+    });
+  }
 };
